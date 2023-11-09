5a2cb943a0dd656baff68b0a48ca7b52a7476e31
Frederik <frederik@pretzmann.dk>
2023-11-09 16:23:12 +0100

    Fixed re.sub error

--- src/ddWrite.py
+++ src/ddWrite.py
@@ -8,13 +8,17 @@ def writeFile(pathOptions):
     txt_input = open(pathOptions.InTXTPath, "r", encoding="utf-8").read().splitlines() 
     txt_output = open(pathOptions.OutTXTPath, "r", encoding="utf-8").read().splitlines() 
 
+
+    def backslashToString(match_obj):
+        return str(txt_input[i])
+
     i = 0
     for line in xml_input:
         if(i < len(txt_input)):
             match_obj = re.search(r"([^\[]+?)(?=]{2})", line) # Search for all text in string
 
             if match_obj is not None: # Error checking to make sure the regex value is not empty
-                match_obj_sub = re.sub(r"([^\[]+?)(?=]{2})", txt_input[i], line)
+                match_obj_sub = re.sub(r"([^\[]+?)(?=]{2})", backslashToString, line)
                 # print(match_obj_sub) # For testing purposes
 
                 if(i == len(txt_input)):
@@ -29,3 +33,5 @@ def writeFile(pathOptions):
                 else:
                     open(pathOptions.OutXMLPath, "a", encoding="utf-8").write(line+"\n")
     # Compare lines in txt_input vs text_output and send warning if they differ
+
+
